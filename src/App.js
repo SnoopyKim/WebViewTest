a0a89d024ce4814595a5d3725da8ff869b06e178
SnoopyKim <snoopykim0@gmail.com>
2022-01-19 11:01:31 +0900

    Bridge 세팅 for React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [token, refreshToken] = window.location.search
+    .substring(1)
+    .split("&")
+    .map((v) => v.split("=")[1]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (token == undefined || refreshToken == undefined) {
+      setLoading(false);
+    } else {
+      setTimeout(() => {
+        setLoading(false);
+        const token = "token!";
+        const refreshToken = "refreshToken!";
+        window.Android.onLogin(token, refreshToken);
+      }, 2000);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +37,24 @@ function App() {
           Learn React
         </a>
       </header>
+      {loading && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#000000CC",
+            color: "white",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Loading...
+        </div>
+      )}
     </div>
   );
 }
