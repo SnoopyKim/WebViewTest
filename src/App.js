037c5d2d1dc975e741b89bce24db414242037cf2
SnoopyKim <snoopykim0@gmail.com>
2022-01-19 13:39:30 +0900

    간단 로그인 폼 추가

--- src/App.js
+++ src/App.js
@@ -9,33 +9,83 @@ function App() {
     .map((v) => v.split("=")[1]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    if (token == undefined || refreshToken == undefined) {
+    if (token === undefined || refreshToken === undefined) {
       setLoading(false);
     } else {
       setTimeout(() => {
         setLoading(false);
-        const token = "token!";
-        const refreshToken = "refreshToken!";
         window.Android.onLogin(token, refreshToken);
       }, 2000);
     }
   }, []);
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      window.Android.onLogin(
+        "TOKEN_VALUE_EXAMPLE",
+        "REFRESH_TOKEN_VALUE_EXAMPLE"
+      );
+    }, 2000);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type={"email"}
+          style={{
+            width: 300,
+            height: 50,
+            borderRadius: 10,
+            paddingLeft: 10,
+            paddingRight: 10,
+            fontSize: 16,
+            outline: "none",
+          }}
+          placeholder="이메일을 입력하세요"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type={"password"}
+          style={{
+            width: 300,
+            height: 50,
+            borderRadius: 10,
+            marginTop: 10,
+            paddingLeft: 10,
+            paddingRight: 10,
+            fontSize: 16,
+            outline: "none",
+          }}
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type={"button"}
+          value={"로그인"}
+          style={{
+            width: 320,
+            height: 50,
+            borderRadius: 10,
+            marginTop: 20,
+            fontSize: 16,
+            fontWeight: "bold",
+            letterSpacing: 2,
+            outline: "none",
+            border: "none",
+            backgroundColor: "steelblue",
+            color: "white",
+          }}
+          onClick={login}
+        />
       </header>
       {loading && (
         <div
